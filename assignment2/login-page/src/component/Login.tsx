6e10a64be8c70ab4f03093e5efb2c5dba63f8861
unknown <ns651143@gmail.com>
2024-12-17 21:24:50 +0530

    loginform

---
+++ assignment2/login-page/src/component/Login.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+   
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <div className="login-header">
+          <div className="cart-icon">
+            <span className="cart-symbol">🛒</span>
+            <span className="cart-arrow">↑</span>
+          </div>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        <div className="forgot-password">
+          <a href="#">Forgot password?</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
